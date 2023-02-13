b68c753ebbd28788b68f1bd63cfafb45cf1cf716
Hana <grace.sambora@gmail.com>
2023-02-12 22:14:52 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {name:"hana", address:"meow city"};
+console.log(employee)
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateEmployee = {...employee}
+    updateEmployee[key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updateEmployee = {...employee}
+    delete updateEmployee[key];
+    return updateEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
